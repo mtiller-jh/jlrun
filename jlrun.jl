2bc18ddd97b219497d9f0e6b5499483575d277ae
Michael M. Tiller <michael.tiller@juliahub.com>
2023-11-07 17:43:32 -0500

    refactor: seems to work

--- jlrun.jl
+++ jlrun.jl
@@ -1,11 +1,25 @@
-using Comonicon
 using Pkg
 
-@main function jlrun(pkg::String)
-    println("Package: $(pkg)")
-    # Run using to get Module loaded
-    eval(Meta.parse("using $(pkg)"))
-    m = eval(Meta.parse(pkg))
-    dir = Pkg.pkgdir(m)
-    println("Package directory: $(dir)")
+if length(ARGS)!=2 && length(ARGS)!=1
+    println("jlrun <package> [<script>]")
+    exit(1)
 end
+
+pkg = ARGS[1]
+script = length(ARGS)==2 ? ARGS[2] : "run"
+
+println("ARGS = $(ARGS)")
+println("ARGV = $(PROGRAM_FILE)")
+println("Package: $(pkg)")
+println("Script: $(script)")
+# Run using to get Module loaded
+eval(Meta.parse("using $(pkg)"))
+m = eval(Meta.parse(pkg))
+dir = Pkg.pkgdir(m)
+println("Package directory: $(dir)")
+scriptfile = joinpath(dir, "scripts", "$(script).jl")
+println("Scriptfile: $(scriptfile)")
+
+ENV["JULIA_PROJECT"] = dir
+include(scriptfile)
+
